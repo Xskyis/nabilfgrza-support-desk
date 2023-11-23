36cfebe4af4670ce02dd42df1a207061fe10ce45
Xskyis <nabilafgarezaafgarza@gmail.com>
2023-11-23 09:44:14 +0700

    first commit

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,12 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const cors = require('cors')
 const PORT = process.env.PORT || 8000
 
 // Connect Database
 connectDB()
 
 const app = express()
-app.use(cors({
-    origin: ["https://support-desk-delta.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
